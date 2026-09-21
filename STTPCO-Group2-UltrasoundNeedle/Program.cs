2790f36f787e1b69189e143531a5caeeb1273d96
mne75 <202207430@post.au.dk>
2026-09-21 10:21:44 +0200

    Add project files.

---
+++ STTPCO-Group2-UltrasoundNeedle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using global::RaspberryPiNetCore.ADC;
+using global::RaspberryPiNetCore.JoySticks;
+using global::RaspberryPiNetCore.LCD;
+using global::RaspberryPiNetCore.TWIST;
+
+
+namespace Raspberry_Pi_Dot_Net_Core_Console_Application3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
